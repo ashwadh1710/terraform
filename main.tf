c0d19c86759964c4eddeaebe88051418814cf280
ashwadh1710 <ashwadh1710@gmail.com>
2025-02-18 21:47:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
-resource "null_resource" "test" {}
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "cidr_blocks" {
+  type    = list(string)
+  default = []
+}
+
+locals {
+  cidrs = jsondecode(file("${path.module}/cidr_blocks.json"))["cidrs"]
+}
+
+output "parsed_cidrs" {
+  value = local.cidrs
+}
